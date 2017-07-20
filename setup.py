c1d1ea184d41cf34f3f78ead283e37daaf0e1461
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2017-07-20 13:52:39 +0200

    Add a setup.py file
    This commit adds a basic setup.py file that allows the package
    to be installed using pip.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='streamlines',
+    version='0.0.0',
+    description='A Python package to manipulate diffusion MRI streamlines',
+    author='Samuel Deslauriers-Gauthier',
+    author_email='sam.deslauriers@gmail.com',
+    url='https://github.com/sdeslauriers/streamlines',
+    packages=['streamlines'],
+    scripts=['scripts/streamlines'])
