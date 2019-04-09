140db5bb37def50a6616770accac6fba601e7d7e
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2019-04-09 11:34:45 +0200

    ENH: change setup from distutils to setuptools
    The current install method is distutils which does not support
    all the features of setuptools, including the declaration of
    dependencies.
    
    This commit changes the setup method to setuptools.

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='streamlines',
     version='0.0.0',
-    description='A Python package to manipulate diffusion MRI streamlines',
+    packages=['streamlines'],
+    scripts=['scripts/streamlines'],
+    url='https://github.com/sdeslauriers/streamlines',
+    license='GPL-3.0',
     author='Samuel Deslauriers-Gauthier',
     author_email='sam.deslauriers@gmail.com',
-    url='https://github.com/sdeslauriers/streamlines',
-    packages=['streamlines'],
-    scripts=['scripts/streamlines'])
+    description='A Python package to manipulate diffusion MRI streamlines',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['nibabel', 'numpy', 'scipy'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.'
+    ),
+)
